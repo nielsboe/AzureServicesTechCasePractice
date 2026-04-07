e02724eaa70a6d923ea85f5678968fc12c112f6f
nielsboe <nielsboesten@live.nl>
2026-04-07 09:44:56 +0200

    wip

--- Infrastructure/AzureServiceBus/AzureServiceBusClient.cs
+++ Infrastructure/AzureServiceBus/AzureServiceBusClient.cs
@@ -2,19 +2,18 @@ using System.Text.Json;
 using Azure.Messaging.ServiceBus;
 using Domain.Interfaces;
 
-namespace Infrastructure.AzureServiceBus
+namespace Infrastructure.AzureServiceBus;
+
+public class AzureServiceBusClient(ServiceBusClient client) : IServiceBusSenderClient
 {
-    public class AzureServiceBusClient(ServiceBusClient client) : IServiceBusSenderClient
+    private readonly ServiceBusClient _client = client;
+
+    public async Task Post<T>(T topic, string task)
     {
-        private readonly ServiceBusClient _client = client;
+        var sender = _client.CreateSender(task);
+        var body = JsonSerializer.Serialize(topic);
+        var sbMessage = new ServiceBusMessage(body);
 
-        public async Task Post<T>(T topic, string task)
-        {
-            var sender = _client.CreateSender(task);
-            var body = JsonSerializer.Serialize(topic);
-            var sbMessage = new ServiceBusMessage(body);
-
-            await sender.SendMessageAsync(sbMessage);
-        }
+        await sender.SendMessageAsync(sbMessage);
     }
 }
