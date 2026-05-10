816bde3992573f395b432142283441512fda668f
nielsboe <nielsboesten@live.nl>
2026-05-10 22:21:38 +0200

    Add swagger

--- Presentations/Program.cs
+++ Presentations/Program.cs
@@ -11,11 +11,9 @@ builder.Services.RegisterInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 
 app.UseInfrastructure();
